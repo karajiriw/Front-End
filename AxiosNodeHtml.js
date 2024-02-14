560d29bb1a90769a02ef21c6ee35fecf8bf89ad7
karajiriw <koki18062547@gmail.com>
2024-02-14 10:22:32 +0700

    .

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -4,8 +4,8 @@ const app = express();
 var bodyParser = require('body-parser');
 const path = require("path");
 
-const base_url = "http://localhost:3000";
-//const base_url = "http://10.104.16.198:3000";
+//const base_url = "http://localhost:3000";
+const base_url = "http://10.104.16.198:3000";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
